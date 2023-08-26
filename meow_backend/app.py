58c202a4caf0a1a8e0871d9584c52c087a8936b0
yaowenjun <yaowenjun@111.com.cn>
2023-08-26 15:45:21 +1000

    set up json echo server

--- meow_backend/app.py
+++ meow_backend/app.py
@@ -1,8 +1,30 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
+from logging.config import dictConfig
+from flask_cors import CORS
+import json
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
+
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/hello", methods=['GET', 'POST'])
 def hello_world():
@@ -18,13 +40,15 @@ def hello_world():
     return jsonifiedContent
 
 @app.route("/echoJson", methods=['GET', 'POST'])
-def echoJson(name: None):
+def echoJson():
     data = request.json
-    jsonifiedContent = jsonify(data)
-    return jsonifiedContent
+    # jsonifiedContent = jsonify(data)
+    app.logger.info('body content: ' + json.dumps(data))
+    return data
 
 @app.route("/echo", methods=['GET', 'POST'])
 def echo():
+    app.logger.info('text is: ' + request.args.get('text', ''))
     return "You said: " + request.args.get('text', '')
 
 @app.route("/download", methods=['GET', 'POST'])
