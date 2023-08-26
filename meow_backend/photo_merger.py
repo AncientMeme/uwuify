654f1d09fc52cac9c784e82ae0fac0cae4ac1d87
yaowenjun <yaowenjun@111.com.cn>
2023-08-26 19:40:59 +1000

    set up image server

--- meow_backend/photo_merger.py
+++ meow_backend/photo_merger.py
@@ -1,8 +1,12 @@
 import cv2 as cv
 import numpy as np
-
 from PIL import Image
+from io import BytesIO
+import requests
+import random
+import os
 
+incremental = 0
 
 def openImage(path):
     image = cv.imread(path)
@@ -19,7 +23,7 @@ def resizeCat(original, cat, ratio):
     ori_width = ori_shape[0]
     ori_height = ori_shape[1]
     min_origin = min(ori_width, ori_height)
-    aspect_ratio = cat.shape[1]/cat.shape[0]
+    aspect_ratio = cat.shape[0]/cat.shape[1]
     resizedCat = cv.resize(cat, (round(min_origin * ratio), round(min_origin * ratio * aspect_ratio)))
     return resizedCat
 
@@ -49,25 +53,49 @@ def overlay_image_alpha(img, img_overlay, x, y, alpha_mask):
 
     img_crop[:] = alpha * img_overlay_crop + alpha_inv * img_crop
 
-
-if __name__ == '__main__':
+def overlayFlow(img, img_overlay_rgba):
     x, y = 10, 10
-    img = np.array(Image.open("/opt/Meow/assesets/demo_photo.jpg"))
-    img_overlay_rgba = np.array(Image.open("/opt/Meow/assesets/selected_cats/cat.1.jpg"))
     img = cv.cvtColor(img, cv.COLOR_RGB2RGBA).copy()
     img_overlay_rgba = cv.cvtColor(img_overlay_rgba, cv.COLOR_RGB2RGBA).copy()
-
-    img_overlay_rgba = resizeCat(img, img_overlay_rgba, 0.8)
+    img_overlay_rgba = resizeCat(img, img_overlay_rgba, 1)
 
     # Perform blending
     alpha_mask = img_overlay_rgba[:, :, 3] / 255.0
-    # print(img_overlay_rgba[:, :, 3].shape)
     img_result = img[:, :, :3].copy()
     img_overlay = img_overlay_rgba[:, :, :3]
     overlay_image_alpha(img_result, img_overlay, x, y, alpha_mask)
 
-    kernel = np.ones((5, 5), np.float32) / 15
-    dst = cv.filter2D(img_result, -1, kernel)
-
     # Save result
-    Image.fromarray(img_result).save("img_result3.jpg")
+    # Image.fromarray(img_result).save("img_result3.jpg")
+    global incremental
+    img_name = str(incremental) + ".jpg"
+    incremental += 1
+    savePath = "static/" + img_name
+    Image.fromarray(img_result).save(savePath)
+    return savePath
+
+def downloadImg(url):
+    # url -->content --> wrap it for BytesIO --> open as Image and then convert
+    img = Image.open(BytesIO(requests.get(url).content)).convert("RGB")
+    img = np.array(img)
+    return img
+
+def findRandomCat():
+    list = os.listdir('static/cropped')
+    return 'static/cropped/' + list[random.randint(0, len(list) - 1)]
+
+def downloadImgs(urls):
+    file_paths = []
+    script_dir = os.path.dirname(__file__)
+    # url -->content --> wrap it for BytesIO --> open as Image and then convert
+    for url in urls:
+        image = downloadImg(url)
+        cat_url = findRandomCat()
+        abs_file_path = os.path.join(script_dir, cat_url)
+        random_cat = np.array(Image.open(abs_file_path))
+        file_path = overlayFlow(image,random_cat)
+        file_paths.append(file_path)
+    return file_paths
+
+if __name__ == '__main__':
+    downloadImgs(['https://i.ytimg.com/vi/L45Ua8weKqs/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC9XUQUbDSSoNC2SwmU10yn5tbCLQ'])
