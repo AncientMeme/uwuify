cc6d7e3cb46502096550435eb506fd923731901f
AncientMeme <hatredwraith@gmail.com>
2023-08-28 00:31:19 +1000

    Refactor uwu translator for better maintainability

--- scripts/translator.js
+++ scripts/translator.js
@@ -1,29 +1,35 @@
+/* Translator Rulesets */
+const rules = [
+    { pattern: /(?:r|l)/g, replacement: "w"},
+    { pattern: /(?:R|L)/g, replacement: "W"},
+    { pattern: /n([aeiou])/g, replacement: "ny$1"},
+    { pattern: /N([AEIOU])/g, replacement: "Ny$1"},
+    { pattern: /th/g, replacement: "f"},
+    { pattern: /ove/g, replacement: "uv"},
+    { pattern: /\bcan\b/g, replacement: "c-can" },
+    { pattern: /!+/g, replacement: () => " " + getRandomElement(["৻(  •̀ ᗜ •́  ৻)", "♡⸜(˶˃ ᵕ ˂˶)⸝♡", "o(*>ω<*)o", "(ﾉ´ヮ´)ﾉ", "( ˶ˆᗜˆ˵ )", "♡⸜(˶˃ ᵕ ˂˶)⸝♡"]) + " " },
+    { pattern: /\?+/g, replacement: () => " " + getRandomElement(["o( ˶^▾^˶ )o", "⸜(｡˃ ᵕ ˂ )⸝♡", "(‘. • ᵕ •. `)", "(づ •. •)?", "◝(ᵔᵕᵔ)◜", "(ㅅ´ ˘ `)"]) + " " },
+    { pattern: /\.+(?=$|\s)/g, replacement: () => " " + getRandomElement(["<(˶ᵔᵕᵔ˶)>", "(*´▽`*)", "( • ᴗ - ) ✧", "(｡>﹏<)", "◝(ᵔᵕᵔ)◜", "(,,>﹏<,,)"]) + " " },
+];
+
 /* Keep track of data */
-var isActive = true
-var textCache = []
+var isActive = true;
+var textCache = [];
 
 /* Get user settings */
 chrome.storage.sync.get(["active"]).then((data)=>{
-    isActive = data.active
-    console.log("Plugin Status:", isActive)
-    changeContent()
+    console.log("uwuify status:", isActive);
+    isActive = data.active;
+    changeContent();
 })
 
-/* Track if the updater is active */
+/* Track if the translator is active */
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if(request) {
-        if (request.msg == "toggled") {
-            console.log("uwu toggled:", request.data)
-            isActive = request.data
-            if (isActive) {
-                changeContent()
-            } else {
-                restoreContent()
-            }
-        }
+    if(request && request.msg == "toggled") {
+        isActive = request.data;
+        isActive ? changeContent() : restoreContent();
     }
 });
-
 
 /*
  * Grabs text containers and UwUify the content
@@ -45,22 +51,23 @@ function containText(element) {
 }
 
 function changeContent() {
-    if (isActive) {
-        let textElements = getTextElements()
-        for (element of textElements) {
-            if (!element.classList.contains("UwU")) {
-                textCache.push([element, element.textContent])
-                element.innerText = uwuifyText(element.innerText) 
-                element.classList.add("UwU")
-            }
-        }
-
-        setTimeout(() => {changeContent()}, 3 * 1000)
+    if (!isActive) {
+        return
     }
+
+    let textElements = getTextElements()
+    for (element of textElements) {
+        if (!element.classList.contains("UwU")) {
+            textCache.push([element, element.textContent])
+            element.innerText = uwuifyText(element.innerText) 
+            element.classList.add("UwU")
+        }
+    }
+
+    setTimeout(() => {changeContent()}, 3 * 1000)
 }
 
 function restoreContent() {
-    console.log("Restore Text")
     for (let i = 0; i < textCache.length; ++i) {
         if (textCache[i][0].classList.contains("UwU")) {
             textCache[i][0].textContent = textCache[i][1]
@@ -69,37 +76,16 @@ function restoreContent() {
     }
 }
 
-function getRandomElement(array) {
-    return array[Math.floor(Math.random() * array.length)];
-}
-
 function uwuifyText(text) {
-    const uwuReplacements = [
-        // ... other replacement rules ...
-        { pattern: /\bcan\b/g, replacement: "c-can" }, // \b ensures whole word matching
-        //{ pattern: /(\w)(\w*)/g, replacement: "$1-$1$2" }, // Stuttering effect
-        { pattern: /!+/g, replacement: () => " " + getRandomElement(["❤️", "৻(  •̀ ᗜ •́  ৻)", "♡⸜(˶˃ ᵕ ˂˶)⸝♡", "o(*>ω<*)o", "(ﾉ´ヮ´)ﾉ", "( ˶ˆᗜˆ˵ )", "♡⸜(˶˃ ᵕ ˂˶)⸝♡"]) + " " },
-        { pattern: /\?+/g, replacement: () => " " + getRandomElement(["🌸", "o( ˶^▾^˶ )o", "⸜(｡˃ ᵕ ˂ )⸝♡", "(‘. • ᵕ •. `)", "(づ •. •)?", "◝(ᵔᵕᵔ)◜", "(ㅅ´ ˘ `)"]) + " " },
-        { pattern: /\.+(?=$|\s)/g, replacement: () => " " + getRandomElement(["<(˶ᵔᵕᵔ˶)>", "(*´▽`*)", "( • ᴗ - ) ✧", "(｡>﹏<)", "◝(ᵔᵕᵔ)◜", "(,,>﹏<,,)"]) + " " },
-    ];
-
     if (text) {
-        text = text.replace(/(?:r|l)/g, "w");
-        text = text.replace(/(?:R|L)/g, "W");
-        text = text.replace(/n([aeiou])/g, "ny$1");
-        text = text.replace(/N([aeiou])/g, "Ny$1");
-        text = text.replace(/th/g, "f");
-        text = text.replace(/ove/g, "uv");
-        // text = text.replace(/!+/g, " " + "❤️".repeat(Math.floor(Math.random() * 3) + 1) + " ");
-        // text = text.replace(/\?+/g, " " + "🌸".repeat(Math.floor(Math.random() * 3) + 1) + " ");
-
-        uwuReplacements.forEach(({pattern, replacement}) => {
-            if (typeof replacement === "function") {
-                text = text.replace(pattern, replacement);
-            } else {
-                text = text.replace(pattern, replacement);
-            }
+        rules.forEach(({pattern, replacement}) => {
+            text = text.replace(pattern, replacement);
         });
         return text;
     }
+}
+
+/* Util functions */
+function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
 }
