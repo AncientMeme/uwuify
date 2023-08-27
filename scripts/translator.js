82ed4da5c3d8d94ddc7a40b09bd665ad0adcf648
AncientMeme <hatredwraith@gmail.com>
2023-08-28 00:16:56 +1000

    Seperated functionality for scripts

--- scripts/translator.js
+++ scripts/translator.js
@@ -1,16 +1,29 @@
 /* Keep track of data */
 var isActive = true
-var updater = null
 var textCache = []
-var imageCache = []
 
 /* Get user settings */
 chrome.storage.sync.get(["active"]).then((data)=>{
     isActive = data.active
     console.log("Plugin Status:", isActive)
     changeContent()
-    changeImages()
 })
+
+/* Track if the updater is active */
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if(request) {
+        if (request.msg == "toggled") {
+            console.log("uwu toggled:", request.data)
+            isActive = request.data
+            if (isActive) {
+                changeContent()
+            } else {
+                restoreContent()
+            }
+        }
+    }
+});
+
 
 /*
  * Grabs text containers and UwUify the content
@@ -90,125 +103,3 @@ function uwuifyText(text) {
         return text;
     }
 }
-
-
-/*
- * Get access to all images on the website
- */
-function getImageElements() {
-    let domElements = document.querySelectorAll("*")
-    let imageElements = Array.from(domElements).filter(isImage)
-    return imageElements
-}
-
-function isImage(element) {
-    const style = getComputedStyle(element);
-    // Disregard hidden elements and images that are too small.
-    if(style.display === 'none' || element.width < 128 || element.height < 128) {
-        return false
-    }
-    // Disregard if the image has no src tag
-    if (!element.getAttribute('src') || element.getAttribute('src') === "") {
-        return false
-    }
-    return element.tagName == "IMG"
-}
-
-function isNewImage(element) {
-    return element.classList.contains("requested") == false
-}
-
-function createImageJson(elements) {
-    var obj = new Object()
-    obj.urls = []
-    for (element of elements) {
-        if (!element.classList.contains("requested")) {
-            obj.urls.push(element.src)
-        }
-    }
-    return JSON.stringify(obj)
-}
-
-function postImages(elements) {
-    // send packets for new images
-    var request = new XMLHttpRequest()
-    request.open("POST", "https://wenjunblog.xyz/echoJson")
-    request.setRequestHeader("Content-Type", "application/json")
-
-    let newImages = Array.from(elements).filter(isNewImage)
-    console.log("new images:", newImages.length)
-    if (newImages.length > 0) {
-        let data = createImageJson(newImages)
-        request.send(data)
-
-        // Receive response
-        request.onreadystatechange = function () {
-            if (request.readyState === 4 && request.status === 200) {
-                let json = JSON.parse(request.response)
-                console.log("Got packet", json)
-                mapImages(json)
-            }
-        }
-    }
-}
-
-function mapImages(json) {
-    let images = getImageElements()
-    for (image of images) {
-        if (!image.classList.contains("requested") && image.getAttribute('src') in json.urls) {
-            let newImage = json.urls[image.getAttribute('src')]
-            imageCache.push([image, image.getAttribute('src'), newImage])
-            image.classList.add("requested")
-        }
-    }
-    console.log("Image Cache:",imageCache)
-}
-
-function changeImages() {
-    console.log("change image owo")
-    if (isActive) {
-        for (image of imageCache) {
-            if (!image[0].classList.contains("has-cat")) {
-                image[0].src = image[2]
-                image[0].srcset = image[2]
-                image[0].classList.add("has-cat")
-            }
-        }
-
-        setTimeout(() => {changeImages()}, 3 * 1000)
-    }
-}
-
-function restoreImages() {
-    for (image of imageCache) {
-        if (image[0].classList.contains("has-cat")) {
-            image[0].src = image[1]
-            image[0].srcset = image[1]
-            image[0].classList.remove("has-cat")
-        }
-    }
-}
-
-/*
- * Track if the updater is active
- */
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if(request) {
-        if (request.msg == "toggled") {
-            console.log("uwu toggled:", request.data)
-            isActive = request.data
-            if (isActive) {
-                changeContent()
-                changeImages()
-            } else {
-                restoreContent()
-                restoreImages()
-            }
-        }
-    }
-});
-
-setInterval(() => {
-    let imageElements = getImageElements()
-    postImages(imageElements)
-}, 3 * 1000)
